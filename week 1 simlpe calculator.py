a2a6cca78c8c25a584e0abec198064f1ba016a53
Rafia-224 <kabeeralmas50@gmail.com>
2026-08-16 15:51:37 -0700

    Add files via upload

---
+++ week 1 simlpe calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().run_cell_magic('markdown', '', '# **WEEK 1 SIMPLE CALCULATOR**\n')
+
+
+# In[4]:
+
+
+get_ipython().run_cell_magic('markdown', '', '# **THIS NOTEBOOK TAKES TWO AND PERFORM ADD, SUB,MULTI and DIV**\n')
+
+
+# In[11]:
+
+
+num1=float(input("enter first number"))
+num2=float(input("enter your second number (not zero)"))
+
+ADD=num1+num2
+print("addition:", ADD)
+SUB=num1+num2
+print("subtraction:" , SUB)
+MULTI=num1+num2
+print("multiplication:" , MULTI)
+DIV=num1+num2
+print("division:" , DIV)
+
+
+# In[ ]:
+
+
+
+
